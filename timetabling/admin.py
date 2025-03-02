ad012fbb9a44464b5abe254855ae666d3c261435
NovaTorpedo <joeodipo84@gmail.com>
2025-03-03 01:18:29 +0300

    admin

--- timetabling/admin.py
+++ timetabling/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Department, School, AcademicYear, Course, Year, Unit, Room, Instructor
 # Register your models here.
+
+admin.site.register(Department)
+admin.site.register(School)
+admin.site.register(AcademicYear)
+admin.site.register(Course)
+admin.site.register(Year)
+admin.site.register(Unit)
+admin.site.register(Room)
+admin.site.register(Instructor)
