3bdc8546320b065c34969455bdf98f3a26ccfdf7
NovaTorpedo <joeodipo84@gmail.com>
2025-03-03 00:15:50 +0300

    index.html

--- timetabling/urls.py
+++ timetabling/urls.py
@@ -4,11 +4,4 @@ from . import views
 
 urlpatterns = [
     path("",views.index, name="index"),
-    path("about", views.about, name="about"),
-    path("booking", views.booking, name="booking"),
-    path("contact",views.contact, name="contact"),
-    path("service", views.service, name="service"),
-    path("team", views.team, name="team"),
-    path("testimonial", views.testimonial, name="testimonial"),
-    path("404", views.not_found, name="404") 
     ]
--- timetabling/views.py
+++ timetabling/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'index.html')
+def index(request):
+    return render(request, 'timetabling/index.html')
